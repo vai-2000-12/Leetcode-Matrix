ead8fb493c5f8d8fdd63cbb9041b076003d8a479
vaibhav Soni <vaibhavsoni1712@gmail.com>
2024-04-04 22:21:02 +0530

    commit

---
+++ Flood fill Agoorithm( Rec).java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor){
+        int color = image[sr][sc];
+        if(color != newColor){
+            dfs(image, sr, sc, color, newColor);
+        }   
+        return image;    
+    }
+    
+    public void dfs(int[][] image, int r, int c, int color, int newColor){
+        if(r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] != color || image[r][c] == newColor){
+            return;
+        }
+        
+        image[r][c] = newColor;
+
+        dfs(image, r + 1, c, color, newColor);
+        dfs(image, r - 1, c, color, newColor);
+        dfs(image, r, c + 1, color, newColor);
+        dfs(image, r, c - 1, color, newColor);
+    }
+}
